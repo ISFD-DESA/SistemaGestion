56bffef76e3e4ec8887ae19077d7483dc943f084
Cristian <barrioscristian.9908@gmail.com>
2024-04-25 21:11:06 -0300

    subo listado

--- clientes/clientes.php
+++ clientes/clientes.php
@@ -14,7 +14,7 @@
         <h1 class="text-center">Listado de Clientes</h1>
         <a class="btn btn-primary" href="agregar.php" role="button">Agregar Cliente</a>
         <div class="container">
-            <table class="table table-light">
+            <table class="table">
                     <tr>
                         <th>ID</th>
                         <th>Apellido</th>
@@ -29,8 +29,37 @@
                         <th>Eliminar</th>
                     </tr>
             <?php
-            //aca hacer el listado de clientes
+                //aca hacer el listado de clientes
+                require_once('../config/conexion.php');
+
+                $sql="SELECT * FROM clientes";
+                $result = $scon->query($sql);
+
             ?>
+
+            <tr>
+                <?php
+                    while($datos=mysqli_fetch_row($result)):
+                ?>
+
+                <!-- Listado -->
+                        <td><?php echo $datos[0]; ?></td>
+                        <td><?php echo $datos[1]; ?></td>
+                        <td><?php echo $datos[2]; ?></td>
+                        <td><?php echo $datos[3]; ?></td>
+                        <td><?php echo $datos[4]; ?></td>
+                        <td><?php echo $datos[5]; ?></td>
+                        <td><?php echo $datos[6]; ?></td>
+                        <td><?php echo $datos[7]; ?></td>
+                        <td><?php echo $datos[8]; ?></td>
+                        <td></td>
+                        <td><a class="btn btn-warning" href="eliminar.php?id=<?php echo $datos[0]; ?>">Eliminar</a></td>
+
+                <?php
+                    endwhile;
+                ?>
+            </tr>
+
         </table>
         </div>
         </div><!--/.container-->
