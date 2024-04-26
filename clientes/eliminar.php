f71db8488fbcdb9b8aa4449aefa93eb4b8d24506
Mariannorio <rodriguezmarianoarg@gmail.com>
2024-04-25 21:20:23 -0300

    subo eliminar

--- clientes/eliminar.php
+++ clientes/eliminar.php
@@ -1,4 +1,29 @@
 <?php
 include '../config/conexion.php';   
 //aca recibir id  y eliminar el registro
+$nombre = $_POST['nombre'];
+$apellido = $_POST['apellido'];
+$dni = $_POST['dni'];
+$sexo = $_POST['sexo'];
+$domicilio = $_POST['domicilio'];
+$telefono = $_POST['telefono'];
+$correo = $_POST['correo'];
+$datos_importantes = $_POST['datos_importantes'];
+
+$sql = "DELETE * FROM usuarios WHERE id_cliente = '$id_cliente'";
+
+if(isset($_GET['id_cliente']) && !empty($_GET['id_cliente'])) {
+    $id = $_GET['id_cliente'];
+
+    if ($scon->query($sql) === TRUE) {
+        echo "Registro eliminado correctamente.";
+    } else {
+        echo "Error al eliminar el registro: " . $scon->error;
+    }
+} else {
+    echo "No se ha proporcionado un ID para eliminar.";
+}
+
+
+$scon->close();
 ?>
--- config/conexion.php
+++ config/conexion.php
@@ -1,6 +1,6 @@
 <?php
 
-$scon = mysqli_connect('server', 'user', 'pass', 'base_datos');
+$scon = mysqli_connect('localhost', 'root', '', 'sistema_bdm');
 
 if (!$scon) {
     die("Conexión fallida: " . mysqli_connect_error());
